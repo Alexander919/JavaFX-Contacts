b4ab82d362293d84b5b0bada5d5fdc6b8c3098f7
Alexander <a.jegorov@inbox.lv>
2019-11-28 22:44:19 +0000

    building application structure

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,30 @@
 package sample;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class Controller {
-    public TableView tableView;
+    public TableView<Contact> tableView;
+    public TextField firstNameField;
+    public TextField lastNameField;
+    public TextField phoneNumberField;
+    public TextField notesField;
+
 
     public void initialize() {
 
+    }
+
+    public void addPerson(ActionEvent actionEvent) {
+        ObservableList<Contact> data = tableView.getItems();
+        data.add(new Contact(firstNameField.getText(), lastNameField.getText(),
+                phoneNumberField.getText(), notesField.getText()));
+
+        firstNameField.setText("");
+        lastNameField.setText("");
+        phoneNumberField.setText("");
+        notesField.setText("");
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,11 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 540, 300));
+        primaryStage.setTitle("Simple Contacts");
+        primaryStage.setScene(new Scene(root, 550, 300));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
