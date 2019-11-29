d2075c9934a46850e4b7b60143ed63a936604144
Alexander <a.jegorov@inbox.lv>
2019-11-29 15:14:27 +0000

    changed fxml array to Controller array

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableView;
@@ -11,10 +12,11 @@ public class Controller {
     public TextField lastNameField;
     public TextField phoneNumberField;
     public TextField notesField;
+    ObservableList<Contact> observableList;
 
 
     public void initialize() {
-
+        setTestingValues();
     }
 
     public void addPerson(ActionEvent actionEvent) {
@@ -26,5 +28,15 @@ public class Controller {
         lastNameField.setText("");
         phoneNumberField.setText("");
         notesField.setText("");
+    }
+
+    public void setTestingValues() {
+        observableList = FXCollections.observableArrayList(
+                new Contact("John", "Smith", "123456", "hello John"),
+                new Contact("Mary", "Doe", "654321", "hello Mary"),
+                new Contact("Isabella", "Williams", "2346541", "hello Isabella"),
+                new Contact("Alex", "Brown", "7890123", "how you doing?")
+        );
+        tableView.setItems(observableList);
     }
 }
