854f39838be661d0a9c0c33b4b2f23aae30fd110
Alexander <a.jegorov84@gmail.com>
2019-12-01 23:24:18 +0000

    made some changes to saveContactList and removed temp data to Array(constructor)

--- src/main/ContactData.java
+++ src/main/ContactData.java
@@ -7,8 +7,10 @@ import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -23,14 +25,14 @@ public class ContactData {
     public static File contactsFile = new File("contacts.dat");
 
     //temporary
-    ContactData() {
-        observableList = FXCollections.observableArrayList(
-                new Contact("John", "Smith", "123456", "hello John"),
-                new Contact("Mary", "Doe", "654321", "hello Mary"),
-                new Contact("Isabella", "Williams", "2346541", "hello Isabella"),
-                new Contact("Alex", "Brown", "7890123", "how you doing?")
-        );
-    }
+//    ContactData() {
+//        observableList = FXCollections.observableArrayList(
+//                new Contact("John", "Smith", "123456", "hello John"),
+//                new Contact("Mary", "Doe", "654321", "hello Mary"),
+//                new Contact("Isabella", "Williams", "2346541", "hello Isabella"),
+//                new Contact("Alex", "Brown", "7890123", "how you doing?")
+//        );
+//    }
 
     static ContactData getInstance() {
         return contactData;
@@ -40,21 +42,26 @@ public class ContactData {
         return observableList;
     }
 
+    //read a path to contacts from location.dat
     public static File readPathFromLocation() throws IOException {
         BufferedReader br = Files.newBufferedReader(location);
         return new File(br.readLine());
     }
 
     public void loadContactList(File file) throws IOException {
+        observableList = FXCollections.observableArrayList();
         //todo Implement loadContactList
         if (file == null) return;
 
         BufferedReader br = new BufferedReader(new FileReader(file));
+        //array to store read data
         ArrayList<String[]> data = new ArrayList<>();
+        //string to store a piece of data
         String piece;
+        //index variable (i % 4)
         int i = 0;
+
         do {
-            //i % 4
             String[] contact = new String[fields];
 
             while (true) {
@@ -68,8 +75,9 @@ public class ContactData {
             if (piece != null) data.add(contact);
 
             System.out.println(Arrays.toString(contact));
-
+            //readLine returns null if the end of the file has been reached
         } while (piece != null);
+
         addContactsToList(data);
     }
 
@@ -79,7 +87,7 @@ public class ContactData {
             observableList.addAll(new Contact(contact[0], contact[1], contact[2], contact[3]));
         }
     }
-
+    //opens a FileChooser depending on 'what' parameter
     public void openFileChooser(BorderPane bp, String what) throws IOException {
         FileChooser fc = new FileChooser();
         Window window = bp.getScene().getWindow();
@@ -89,7 +97,8 @@ public class ContactData {
             file = fc.showSaveDialog(window);
             if (file != null) {
                 //set location of the file
-                contactsFile = file;
+                setPaths(file);
+//                contactsFile = file;
             }
             saveContactList(file);
         } else {
@@ -98,16 +107,22 @@ public class ContactData {
         }
     }
 
+    public void setPaths(File file) throws IOException {
+        contactsFile = file;
+        //remove all contents of location.dat to keep it a one-liner
+        BufferedWriter bw = Files.newBufferedWriter(location, StandardOpenOption.TRUNCATE_EXISTING);
+        //update the path to contacts in location.dat
+        bw.write(file.getAbsolutePath());
+        bw.flush();
+    }
+
     public void saveContactList(File file) throws IOException {
         //user pressed cancel
         if (file == null) return;
-
+        //open contacts file for reading
         PrintWriter pwContacts = new PrintWriter(new BufferedWriter(new FileWriter(file)));
 
-        BufferedWriter bw = Files.newBufferedWriter(location, StandardOpenOption.TRUNCATE_EXISTING);
-        bw.write(file.getAbsolutePath());
-        bw.flush();
-
+        //save contacts to a file
         for (Contact contact : observableList) {
             pwContacts.write(String.format("%s\n%s\n%s\n%s\n\n", contact.getFirstName(),
                     contact.getLastName(), contact.getPhoneNumber(), contact.getContactNotes()));
--- src/main/Main.java
+++ src/main/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
 
     @Override
     public void stop() throws Exception {
-        ContactData.getInstance().saveContactList(ContactData.contactsFile);
+        ContactData.getInstance().saveContactList(ContactData.readPathFromLocation());
     }
 
     @Override
